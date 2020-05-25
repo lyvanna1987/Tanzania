664175fd71cec383568c2ab3c968844df98da24e
Ly Vanna <lyvanna1987@outlook.com>
2020-05-26 05:27:21 +0700

    update creating signup

--- src/Pages/Auth/SignUp.js
+++ src/Pages/Auth/SignUp.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import bg from '../../assets/utils/images/laptop.png'
 
 class SignUp extends React.Component {
 
     render () {
-        return (
-            <div>
-                Hello div....
-            </div>
+        return (            
+            <Container>
+                {/* <Widget></Widget> */}
+                
+            </Container>           
         )
     }
 }
 
 export default SignUp
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    flex-direction: row;
+    background: url(${bg});
+`
+const Widget = styled.div`
+    flex: 1;
+`
+
+
+
